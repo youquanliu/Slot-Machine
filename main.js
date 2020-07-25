5938e3e4ee02bb54177655db91209cea6c92373a
unknown <youquan.liu@hotmail.com>
2020-07-24 23:26:01 -0400

    add audio to chef

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+//***********------------ FLYING KENNY AND STARS ------------************* */
+
 document.addEventListener("mousemove", function (e) {
     let kenny = document.querySelector(".kenny");
     kenny.style.left = e.offsetX + 'px';
@@ -25,3 +28,38 @@ function stars() {
     }
 }
 stars();
+
+//***********------------ CONSTANTS ------------************* */
+const imagesObj = [
+    { name: 'cartman', image: 'img/cartman.png' },
+    { name: 'goCows', image: 'img/goCows.png' },
+    { name: 'jesus', image: 'img/jesus.png' },
+    { name: 'jimmy', image: 'img/jimmy.png' },
+    { name: 'kyle', image: 'img/kyle.png' },
+    { name: 'satan', image: 'img/satan.png' },
+    { name: 'stan', image: 'img/stan.png' },
+    { name: 'timmy', image: 'img/timmy.png' },
+];
+
+const hightlightArr = [w1, w2, w3, w4, l1, l2];
+
+//***********------------ VARIABLES ------------************* */
+
+let tokens;
+let spinResults = [];
+
+//***********------------ REFERENCES ------------************* */
+
+let betInput = document.querySelector('#betInput');
+let betBtn = document.querySelector('#betBtn');
+let roller = document.querySelector('#spinner-section');
+let quitBtn = document.querySelector('#quit');
+let helloChildren_audio = document.getElementById('helloChildren');
+// let win_audio = document.querySelector('#');
+let body = document.querySelector('.chef-body');
+/* * * * * * * * * * * EVENT LISTENERS * * * * * * * * * * * */
+
+body.addEventListener('click', function () {
+    helloChildren_audio.play();
+    console.log("h")
+}); 
