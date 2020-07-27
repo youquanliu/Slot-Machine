240e41540fd02f2559a9be03f6d8a4e9e021c6e7
unknown <youquan.liu@hotmail.com>
2020-07-27 15:28:40 -0400

    Add 1.function to traslate $ to tokens 2.function spin to get random images and set to rollers

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function stars() {
 stars();
 
 //***********------------ CONSTANTS ------------************* */
-const imagesObj = [
+const imagesObjArray = [
     { name: 'cartman', image: 'img/cartman.png' },
     { name: 'goCows', image: 'img/goCows.png' },
     { name: 'jesus', image: 'img/jesus.png' },
@@ -45,7 +45,7 @@ const hightlightArr = [w1, w2, w3, w4, l1, l2];
 
 //***********------------ VARIABLES ------------************* */
 
-let tokens;
+let tokensAmount;
 let spinResults = [];
 
 //***********------------ REFERENCES ------------************* */
@@ -53,13 +53,92 @@ let spinResults = [];
 let betInput = document.querySelector('#betInput');
 let betBtn = document.querySelector('#betBtn');
 let roller = document.querySelector('#spinner-section');
-let quitBtn = document.querySelector('#quit');
+let quitBtn = document.querySelector('#quitBtn');
 let helloChildren_audio = document.getElementById('helloChildren');
 // let win_audio = document.querySelector('#');
 let body = document.querySelector('.chef-body');
-/* * * * * * * * * * * EVENT LISTENERS * * * * * * * * * * * */
+let tokens = document.querySelector('#tokens');
+const imageTags = document.querySelectorAll(".roller > img");  //object nodelist of image
 
+
+//***********------------ EVENT LISTENER ------------************* */
+
+betBtn.addEventListener('click', placeBet);
+quitBtn.addEventListener('click', initialize);
+roller.addEventListener('click', spin);
+//Hello there children
 body.addEventListener('click', function () {
     helloChildren_audio.play();
-    console.log("h")
-}); 
+});
+
+
+//***********------------ FUNCTIONS ------------************* */
+
+initialize();
+
+function initialize() {
+    tokensAmount = 0;
+    tokens.innerText = '000000';
+    hightLightRemover();
+    for (let index = 0; index < 3; index++) {
+        imageTags[index].src = imagesObjArray[Math.floor(Math.random() * imagesObjArray.length)].image;
+        imageTags[index].style.width = '81%';
+    } inputInitializer()
+}
+
+function placeBet() {
+    if (!isNaN(betInput.value) && betInput.value.length > 0) {
+        tokensAmount += Math.floor(betInput.value) / 0.05;
+        tokens.innerHTML = sixDigit(tokensAmount);
+        betInput.placeholder = '$0.00';
+        inputInitializer();
+    }
+    else {
+        betInput.placeholder = "Don't be mean";
+        inputInitializer();
+    }
+}
+
+function spin() {
+    //spin sound play;
+    if (tokens.innerHTML > 0) {
+        hightLightRemover();
+        tokensAmount -= 10;
+        tokens.innerHTML = sixDigit(tokensAmount);
+        for (let index = 0; index < 3; index++) {
+            spinResults[index] = imagesObjArray[Math.floor(Math.random() * imagesObjArray.length)].image;
+        }
+        setImageToRoller(spinResults);
+    }
+    else betInput.placeholder = "You are out of money";
+}
+
+function setImageToRoller(imgObj) {
+    let t = 0;
+    for (let index = 0; index < imageTags.length; index++) {
+        setTimeout(() => {
+            imageTags[index].src = imgObj[index]
+        }, t += 300);
+    }
+}
+
+
+
+
+
+function inputInitializer() {
+    betInput.value = '';
+    betInput.style = null;
+}
+
+function sixDigit(number) {
+    if (number <= 9999) { number = ("00000" + number).slice(-6); }
+    return number;
+}
+
+function hightLightRemover() {
+    for (let index = 0; index < 3; index++) {
+        hightlightArr[index].style = null;
+    }
+    console.log("hightLightRemover worked");
+}
