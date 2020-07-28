0685b5a9b9bee3203330fbd9dffa1953c8e30ecb
unknown <youquan.liu@hotmail.com>
2020-07-27 23:15:50 -0400

    add rules for add or reduce token amount according to rolling result

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ function stars() {
     while (i < count) {
         let star = document.createElement('i');
         let x = Math.floor(Math.random() * window.innerWidth);
-        let duration = Math.random() * 1.2;
+        let duration = Math.random() * 5;
         let h = Math.random() * 100;
 
         star.style.left = x + 'px';
-        star.style.width = 1 + 'px';
+        star.style.width = 2 + 'px';
         //length of star
-        star.style.height = 60 + h + 'px';
+        star.style.height = 40 + h + 'px';
         star.style.animationDuration = duration + 's';
         star.style.zIndex = -1;
 
@@ -27,13 +27,12 @@ function stars() {
         i++;
     }
 }
+
 stars();
 
 //***********------------ CONSTANTS ------------************* */
 const imagesObjArray = [
     { name: 'cartman', image: 'img/cartman.png' },
-    { name: 'goCows', image: 'img/goCows.png' },
-    { name: 'jesus', image: 'img/jesus.png' },
     { name: 'jimmy', image: 'img/jimmy.png' },
     { name: 'kyle', image: 'img/kyle.png' },
     { name: 'satan', image: 'img/satan.png' },
@@ -79,11 +78,13 @@ initialize();
 function initialize() {
     tokensAmount = 0;
     tokens.innerText = '000000';
+    betInput.placeholder = '$0.00';
     hightLightRemover();
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < imageTags.length; index++) {
         imageTags[index].src = imagesObjArray[Math.floor(Math.random() * imagesObjArray.length)].image;
         imageTags[index].style.width = '81%';
-    } inputInitializer()
+    }
+    inputInitializer()
 }
 
 function placeBet() {
@@ -105,7 +106,7 @@ function spin() {
         hightLightRemover();
         tokensAmount -= 10;
         tokens.innerHTML = sixDigit(tokensAmount);
-        for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < imageTags.length; index++) {
             spinResults[index] = imagesObjArray[Math.floor(Math.random() * imagesObjArray.length)].image;
         }
         setImageToRoller(spinResults);
@@ -118,11 +119,57 @@ function setImageToRoller(imgObj) {
     for (let index = 0; index < imageTags.length; index++) {
         setTimeout(() => {
             imageTags[index].src = imgObj[index]
-        }, t += 300);
-    }
+        }, t += 200);
+    };
+    rule(spinResults);
 }
 
-
+function rule(arr) {
+    var count = {};
+    arr.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
+    for (let key in count) {
+        if (count[key] == 2) {
+            if (key == "img/stan.png") {
+                tokensAmount += 50;
+                tokens.innerHTML = sixDigit(tokensAmount);
+                w1.style.background = 'yellow';
+                w1.style.color = 'blue';
+            }
+            else if (key == "img/kyle.png") {
+                tokensAmount += 100;
+                tokens.innerHTML = sixDigit(tokensAmount);
+                w3.style.background = 'yellow';
+                w3.style.color = 'blue';
+            }
+            else if (key == "img/satan.png") {
+                tokensAmount *= 0.4;
+                tokens.innerHTML = sixDigit(tokensAmount);
+                l1.style.background = 'yellow';
+                l1.style.color = 'blue';
+            }
+            else if (key == "img/timmy.png") {
+                tokensAmount = Math.floor(tokensAmount * 1.2);
+                tokens.innerHTML = sixDigit(tokensAmount);
+                w4.style.background = 'yellow';
+                w4.style.color = 'blue';
+            }
+        }
+        else if (count[key] == 3 && key == "img/cartman.png") {
+            //audio
+            tokensAmount -= 300;
+            tokens.innerHTML = sixDigit(tokensAmount);
+            l2.style.background = 'yellow';
+            l2.style.color = 'blue';
+        }
+        else if (count[key] == 3) {
+            //audio
+            tokensAmount += 500;
+            tokens.innerHTML = sixDigit(tokensAmount);
+            w2.style.background = 'yellow';
+            w2.style.color = 'blue';
+        }
+    }
+}
 
 
 
@@ -137,8 +184,9 @@ function sixDigit(number) {
 }
 
 function hightLightRemover() {
-    for (let index = 0; index < 3; index++) {
-        hightlightArr[index].style = null;
+
+    for (i = 0; i < hightlightArr.length; i++) {
+        hightlightArr[i].style = null;
     }
     console.log("hightLightRemover worked");
 }
